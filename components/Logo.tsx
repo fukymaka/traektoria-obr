b4ac1cea992fcdbd72966eb7349d7987fb30c520
Дорожкин Роман Владимирович <dorr@omega.local>
2024-04-24 21:16:27 +0300

    add logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+
 export default function Logo({ ...rest }) {
   return (
-    <h2>Траектория Образования</h2>
+    <LogoContainer>
+      <LogoImage src='./logo_to.png' alt='Company Logo' />
+      <h2>Траектория Образования</h2>
+    </LogoContainer>
   );
 }
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+`;
+
+const LogoImage = styled.img`
+  margin-right: 20px;
+`;
